1fd6caa6e72a048478b35008163c43fdb20a6476
muxudiinkeey <57723477+muxudiinkeey@users.noreply.github.com>
2023-06-07 12:41:38 +0200

    kumantarkii koowaad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,31 @@ import { UserComponent } from './user/user.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
+
+import { WelcomeComponent } from './homme/welcome/welcome.component';
+import { ProductListComponent } from './products/product-list.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    WelcomeComponent,
+    ProductListComponent,
+    ProductDetailComponent
+
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+
+  BrowserModule,
+  AppRoutingModule,
+
   ],
   providers: [],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
